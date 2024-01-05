a012cf61d57335af305231a5af1e35a2473d9181
whs31 <75864292+whs31@users.noreply.github.com>
2024-01-06 02:09:18 +0300

    Integrated with conan

---
+++ build.py
@@ -0,0 +1,28 @@
+import subprocess
+
+
+def run_conan(build_type):
+    subprocess.run(
+        (
+            'conan', 'install', '.',
+            '--profile', 'conan/profiles/mingw_windows_x86_64',
+            '--build', 'missing',
+            '--output-folder=./dependencies',
+            f'--settings=build_type={build_type}'
+        )
+    )
+
+
+# def run_cmake():
+#     subprocess.run(
+#         (
+#             'cmake', '..',
+#         )
+#     )
+
+
+if __name__ == '__main__':
+    run_conan('Debug')
+    run_conan('Release')
+
+
--- conanfile.py
+++ conanfile.py
@@ -2,18 +2,12 @@ from conan import ConanFile
 
 
 class Corona(ConanFile):
-    generators = ("CMakeToolchain", "CMakeDeps")
-    settings = ("os", "compiler", "build_type", "arch")
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    name = "Corona"
+    version = "1.0.0"
 
     def requirements(self):
-        self.requires("nlohmann_json/3.11.2")
-
-    def build_requirements(self):
-        # self.tool_requires("cmake/3.25.0")
-        pass
-
-    def configure(self):
-        pass
-
-    def layout(self):
-        self.folders.generators = ""
+        self.requires("spdlog/1.11.0")
+        self.requires("yaml-cpp/0.8.0")
