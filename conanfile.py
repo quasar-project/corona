ee58c35a31771eb5aa8b97205be37103fbba018a
whs31 <75864292+whs31@users.noreply.github.com>
2024-01-06 03:15:39 +0300

    added msvc profile

--- conanfile.py
+++ conanfile.py
@@ -9,22 +9,22 @@ class Corona(ConanFile):
     version = "1.0.0"
 
     def requirements(self):
-        # self.requires("qt/6.6.1")
+        self.requires("qt/6.6.1")
         self.requires("spdlog/1.11.0")
         self.requires("yaml-cpp/0.8.0")
 
 
-        # self.default_options = {
-        #     "qt:shared": True,
-        #     "qt:qtsvg": True,
-        #     "qt:qtquickcontrols2": True,
-        #     "qt:qtnetwork": True,
-        #     "qt:qtlocation": True,
-        #     "qt:qtdeclarative": True,
-        #     "qt:gui": True,
-        #     "qt:qtpositioning": True,
-        #     "qt:qtimageformats": True,
-        #
-        #     "qt:with_harfbuzz": False,
-        #     "qt:with_glib": False,
-        # }
+        self.default_options = {
+            "qt:shared": True,
+            "qt:qtsvg": True,
+            "qt:qtquickcontrols2": True,
+            "qt:qtnetwork": True,
+            "qt:qtlocation": True,
+            "qt:qtdeclarative": True,
+            "qt:gui": True,
+            "qt:qtpositioning": True,
+            "qt:qtimageformats": True,
+
+            "qt:with_harfbuzz": False,
+            "qt:with_glib": False,
+        }
