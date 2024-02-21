11149ef74cab7703cba7041cf18cf9fb6d99ec16
whs31 <75864292+whs31@users.noreply.github.com>
2024-02-21 21:42:45 +0300

    Update powerswitch.h

--- src/c++/network/modules/powerswitch.h
+++ src/c++/network/modules/powerswitch.h
@@ -1,8 +1,30 @@
-//
-// Created by user on 21.02.2024.
-//
+#pragma once
 
-#ifndef POWERSWITCH_H
-#define POWERSWITCH_H
+#include <leaf/global.h>
 
-#endif //POWERSWITCH_H
+namespace network::modules
+{
+  using namespace leaf::types;
+
+  class PowerSwitch
+  {
+    constexpr u32 REQUEST_MARKER = 0xAAAAAAAA;
+
+    struct [[gnu::packed]] RequestPacket
+    {
+      u32 marker;
+      u16 command;
+      u16 port;
+      u16 checksum;
+    };
+
+    struct [[gnu::packed]] ResponsePacket
+    {
+      u16 header;
+      u8 channel;
+      u8 enabled;
+      u32 voltage;
+      u32 current;
+    };
+  };
+}
