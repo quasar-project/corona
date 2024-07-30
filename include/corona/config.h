0b2fd21b20d20659b7b5efb4bc5fc96534637e33
whs31 <ryazancev.dl@edu.spbstu.ru>
2024-07-30 09:22:16 +0300

    Powerswitch config

---
+++ include/corona/config.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <vector>
+#include <floppy/configuration.h>
+#include <corona/detail/export.h>
+
+namespace corona
+{
+  struct ConfigData
+  {
+    struct PowerSwitch
+    {
+      std::vector<string> channel_names = { "АПД", "РЛМ", "ЭВМ", "УМ", "К5", "КОМ", "НАВ" };
+
+      struct Address
+      {
+        string ip = "192.168.1.50";
+        u16 port = 44000;
+      } address;
+
+      struct Gauges
+      {
+        i32 cell_count = 5;
+        f32 cell_min_voltage_volts = 3.2F;
+        f32 cell_max_voltage_volts = 4.3F;
+        f32 max_current_amperes = 7.0F;
+      } gauges;
+
+      std::vector<std::pair<f32, f32>> channel_voltages = {
+        { 10.0F, 20.0F },
+        { 10.0F, 20.0F },
+        { 10.0F, 20.0F },
+        { 10.0F, 20.0F },
+        { 10.0F, 20.0F },
+        { 0.0F, 10.0F },
+        { 0.0F, 10.0F }
+      };
+    };
+  };
+} // namespace corona
+
+namespace floppy::serialization
+{
+  template <> auto serialize<format::yaml>(corona::ConfigData const& value) -> std::string;
+  template <> auto deserialize<format::yaml>(std::string const& value) -> corona::ConfigData;
+} // namespace floppy::serialization
